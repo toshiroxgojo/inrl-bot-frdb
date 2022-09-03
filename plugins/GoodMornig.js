99f8899b7dc0188150e40baf72d0600fb7dc75a3
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-09-03 15:44:44 +0530

    Fixed

---
+++ plugins/GoodMornig.js
@@ -0,0 +1,35 @@
+const bots = require("../lib/perfix");
+const GDM = "it sends good morning message";
+const GDN = "it sends Night message";
+
+bots.bot(
+  { pattern: ["gm"], desc: GDM, sucReact: "💖", category: ["chat", "all"] },
+  async (message, client) => {
+    var r_text = new Array();
+    r_text[0] = "❀🍃Good❀ ❀morning❀🥰❀ ";
+    r_text[1] = "☘️𝐺𝑜𝑜𝑑 🌅𝑚𝑜𝑟𝑛𝑖𝑛𝑔 💐 ";
+    r_text[2] = "🍃𝙶𝚘𝚘𝚍 🌻𝚖𝚘𝚛𝚗𝚒𝚗𝚐 🥰 ";
+    r_text[3] = "🍀𝗚𝗼𝗼𝗱 😘𝗺𝗼𝗿𝗻𝗶𝗻𝗴 🌸 ";
+    r_text[4] = "🌻𝓖𝓸𝓸𝓭 𝓶𝓸𝓻𝓷𝓲𝓷𝓰 💞 ";
+    r_text[5] = "🌼🅖🅞🅞🅓 🅜🅞🅡🅝🅘🅝🅖 🐶 ";
+    r_text[6] = "🍃Ⓖⓞⓞⓓ 🌈ⓜⓞⓡⓝⓘⓝⓖ 🥰 ";
+    const i = Math.floor(7 * Math.random());
+    await client.sendMessage( message.from, { text: r_text[i] + message.client.pushName }, { quoted: message } );
+    global.catchError = false;
+  }
+);
+
+bots.bot(
+  { pattern: ["gn"], desc: GDN, sucReact: "💖", category: ["chat", "all"] },
+  async (message, client) => {
+    var r_text = new Array();
+    r_text[0] = "😘𝙂𝙤𝙤𝙙 🙈𝙣𝙞𝙜𝙝𝙩 💫✨";
+    r_text[1] = "🤗𝓖𝓸𝓸𝓭 🧚‍♀𝓷𝓲𝓰𝓱𝓽 ❄️✨";
+    r_text[1] = "🌌❡០០ᖱ 🌙⩎ɨ❡ϦƬ 🌎";
+    r_text[3] = "😘ցօօժ ⭐️ղíցհԵ 💝";
+    r_text[4] = "🌃Ꮐᝪᝪᗞ 🙈ᑎᏆᏀᕼᎢ 💫✨";
+    const i = Math.floor(5 * Math.random());
+    await client.sendMessage( message.from, { text: r_text[i] + message.client.pushName }, { quoted: message, adReply: true } );
+    global.catchError = false;
+  }
+);
