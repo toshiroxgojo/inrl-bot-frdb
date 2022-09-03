b228a373398458ad32723d27de8cd4de866c3f96
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-09-03 23:57:22 +0530

    Fixed

--- plugins/Ping.js
+++ plugins/Ping.js
@@ -7,15 +7,10 @@ bots.bot(
                 sucReact: "🥱",
 	   },
 	async (message, client) => {
-	try {
 		const start = new Date().getTime()
 		await client.sendMessage( message.from, { text: 'Ping!' }, { quoted: message })
 		const end = new Date().getTime()
 		return await client.sendMessage( message.from, { text: 'Pong!\n' + (end - start) + 'ms' }, { quoted: message })
 		global.catchError = false;
-		 } catch (error) {
-      global.catchError = true;
-      return await client.sendErrorMessage( message.from, error, message.key, message );
-       }
 	 }
-)
+);
