3f335dee395e50f83b91d165bd4063b3ba6503f1
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-09-12 19:27:47 +0530

    Fix❣️🥵🤣

--- global.js
+++ global.js
@@ -1,10 +1,10 @@
-const fs = require("fs")
-global.user = require("./lib/database/data/user")
-global.group = require("./lib/database/data/group")
-global._user = JSON.parse(fs.readFileSync("./lib/database/data/user.json"))
-global._group = JSON.parse(fs.readFileSync("./lib/database/data/group.json"))
-global.owner = ["917593919575"];
-global.mess = (type, m) => {
-    let msg = { wait: 'Wait, in progress', owner: 'only for owner', premium: 'onpy for primum mebers, group: 'only in grp', private: 'only in personel chat', admin: 'only for grp dmin', botAdmin: 'onpy for bot owner', bot: 'only for bot numbert', dead: 'bot deqd😇🤣!', media: 'Reply media', error: "No Results Found" }[type]
-    if (msg) return m.reply(msg, m.from, { quoted: m })
-}
+/* Copyright (C) 2020 INrlTeam.
+
+Licensed under the  GPL-3.0 License;
+
+you may not use this file except in compliance with the License.
+
+inrl-md
+
+*/
+function _0x5e93(_0x12be70,_0x10b726){const _0x102932=_0x1029();return _0x5e93=function(_0x5e932c,_0x3f635f){_0x5e932c=_0x5e932c-0x85;let _0x31d0bc=_0x102932[_0x5e932c];return _0x31d0bc;},_0x5e93(_0x12be70,_0x10b726);}const _0x452bc9=_0x5e93;(function(_0x7e494,_0x5a930b){const _0x50f8be=_0x5e93,_0x49a8b1=_0x7e494();while(!![]){try{const _0x3b9a6a=-parseInt(_0x50f8be(0x9e))/0x1*(parseInt(_0x50f8be(0xa2))/0x2)+-parseInt(_0x50f8be(0x8d))/0x3*(parseInt(_0x50f8be(0x96))/0x4)+-parseInt(_0x50f8be(0x8a))/0x5*(-parseInt(_0x50f8be(0x90))/0x6)+parseInt(_0x50f8be(0xa3))/0x7*(-parseInt(_0x50f8be(0x8b))/0x8)+-parseInt(_0x50f8be(0xa1))/0x9*(parseInt(_0x50f8be(0x85))/0xa)+parseInt(_0x50f8be(0x95))/0xb+parseInt(_0x50f8be(0x98))/0xc;if(_0x3b9a6a===_0x5a930b)break;else _0x49a8b1['push'](_0x49a8b1['shift']());}catch(_0x3f8950){_0x49a8b1['push'](_0x49a8b1['shift']());}}}(_0x1029,0x32e2f));function _0x1029(){const _0x107522=['2288amWzex','Reply\x20media','12ZvWsvF','only\x20for\x20owner','./lib/database/data/user.json','152478yIRHfF','bot\x20deqd😇🤣!','_user','_group','Wait,\x20in\x20progress','961290mdnLSA','326504GSeWfw','./lib/database/data/user','10753956xhdlit','./lib/database/data/group.json','onpy\x20for\x20primum\x20mebers','./lib/database/data/group','only\x20in\x20grp','parse','129ElPdLJ','user','owner','81potLsF','4650Afwald','6580uQTwAg','readFileSync','64160rnbLNM','from','group','No\x20Results\x20Found','onpy\x20for\x20bot\x20owner','35KYKKFV'];_0x1029=function(){return _0x107522;};return _0x1029();}const fs=require('fs');global[_0x452bc9(0x9f)]=require(_0x452bc9(0x97)),global[_0x452bc9(0x87)]=require(_0x452bc9(0x9b)),global[_0x452bc9(0x92)]=JSON[_0x452bc9(0x9d)](fs[_0x452bc9(0xa4)](_0x452bc9(0x8f))),global[_0x452bc9(0x93)]=JSON[_0x452bc9(0x9d)](fs[_0x452bc9(0xa4)](_0x452bc9(0x99))),global[_0x452bc9(0xa0)]=['917593919575'],global['mess']=(_0x5842da,_0x50f8e1)=>{const _0x193b85=_0x452bc9;let _0x2927b5={'wait':_0x193b85(0x94),'owner':_0x193b85(0x8e),'premium':_0x193b85(0x9a),'group':_0x193b85(0x9c),'private':'only\x20in\x20personel\x20chat','admin':'only\x20for\x20grp\x20dmin','botAdmin':_0x193b85(0x89),'bot':'only\x20for\x20bot\x20numbert','dead':_0x193b85(0x91),'media':_0x193b85(0x8c),'error':_0x193b85(0x88)}[_0x5842da];if(_0x2927b5)return _0x50f8e1['reply'](_0x2927b5,_0x50f8e1[_0x193b85(0x86)],{'quoted':_0x50f8e1});};
