a51a4b1efb53b093cfcb705e7e3d7c32fb4d4a32
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-09-14 21:21:49 +0530

    Fix

---
+++ plugins/alive.js
@@ -0,0 +1,23 @@
+const { inrl } = require("../lib/perfix");
+const Config = require("../config");
+inrl(
+  {
+    pattern: ["alive", "bot", "system_status"],
+    desc: "to check the bot status,
+    sucReact: "🥰",
+    category: ["system", "all"],
+  },
+  async (message, client) => {
+const Message = {
+      image: { url: inrl.config.image.url.D_E_TMB },
+      caption:`╭───────────────────
+┃☞︎︎︎│ *ɪᴀᴍ ᴀʟɪᴠᴇ ʙʀᴏ*
+┃∞│ ─━━━━━━━━━━━━━─
+┃☞︎︎︎│ *➪ ɪɴʀʟ-ʙᴏᴛ-ᴍᴅ* 
+┃ɢɪᴛ :`+Config.GIT+`
+┃ꜱᴜᴩᴩᴏʀᴛ ɢʀᴩ: `+Config.WAGRP+`
+┃ᴡᴇʙ :`+Config.WEB+`
+┃∞│ ─━━━━━━━━━━━━━─
+╰───────────────────`
+    };
+    await client.sendMessage(message.from, Message, { quoted: message });
