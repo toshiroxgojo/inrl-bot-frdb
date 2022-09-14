037c95894d1c42e312476b3e617805db56c412fc
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-09-14 21:24:01 +0530

    Fix

---
+++ plugins/github.js
@@ -0,0 +1,13 @@
+const { inrl } = require("../lib/perfix");
+const Config = require("../config");
+inrl(
+  {
+    pattern: ["git"],
+    desc: "to check the bot status,
+    sucReact: "🥰",
+    category: ["system", "all"],
+  },
+  async (message, client) => {
+const msg = Config.GIT
+    await client.sendMessage(message.from, msg, { quoted: message });
+});
