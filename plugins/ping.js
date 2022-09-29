b1edb0f96ba172377204bd438fc1680c984db99a
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-09-29 18:59:29 +0530

    added, 😹💗💖

--- plugins/ping.js
+++ plugins/ping.js
@@ -20,3 +20,13 @@ inrl(
                 }
 	 }
 );
+const InRl = require('../lib/Store');
+
+
+inrl({ pattern: ['del'], desc: "to create to delete unwanted grp msg by admins",sucReact: "⚒️",  category: ["all"]}, async (message, client) => {
+
+                if (!message.quoted) return await client.sendMessage(message.from, { text :"replay to a group content"},{ quoted: message })
+                let { chat, fromMe, id } = message.quoted
+                client.sendMessage(message.from, { delete: { remoteJid: message.chat, fromMe: message.quoted.fromMe, id: message.quoted.id, participant: message.quoted.sender }})
+            }
+);
