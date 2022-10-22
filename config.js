161714176b5fdb0e4392dbcc3d2e7cfdf00c188f
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-10-22 06:48:57 +0530

    Added, moreBut

--- config.js
+++ config.js
@@ -17,7 +17,12 @@ module.exports = {
     BGMBOT : process.env.BGMBOT || 'false',
     WORKTYPE: process.env.WORKTYPE || 'public',
     PM_BLOCK : process.env.PM_BLOCK || "false",
+    WELCOME_SET : process.env.WELCOME_SET || "true", //now thi scmd is not creted creating
+    EXIT_MSG : process.env.EXIT_MSG || "true", //now thi scmd is not creted creating
     CALL_BLOCK : process.env.CALL_BLOCK || "false",
+    ANTI_LINK_ALL : process.env.ANTI_LINK_ALL || "false", //now thi scmd is not creted creating
+    FACK_REMOVE : process.env.FACK_REMOVE || "2222", //now thi scmd is not creted creating
+    SET_ANTI_LINK : process.env.SET_ANTI_LINK || "instagram.com", "xnxx.com", //now thi scmd is not creted creating
     LANG :process.env.LANG || 'EN', //values are ml, en only
     OWNER : process.env.OWNER || "917593919575",
     BRANCH: 'master',
