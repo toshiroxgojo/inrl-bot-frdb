922ec60fa7ae7d6f1e0cbcd3d335ba9aa32728c0
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-01 18:50:02 +0530

    Update toUrl.js

--- plugins/toUrl.js
+++ plugins/toUrl.js
@@ -33,7 +33,7 @@ let text = message.client.text;
 if(text.includes(' ')){ text = text.trim() }
 let imagee = Config.AUDIO_DATA;
 if(imagee.includes(' ')){ imagee = img.trim() }
-if(text.includes(',')){ let img =text.split(',')[2] || imagee.split(',')[2] }
+if(text.includes(',') && text.split(',')[2] !== undefined ){ let img = text.split(',')[2] } else { img = imagee.split(',')[2] }
 console.log("error="+img);
 let imgForUdio = await urlBufferToImgFile(img,'./media/imagForAudio.jpg');
     await AudioMetaData(imgForUdio, media, message, client);
