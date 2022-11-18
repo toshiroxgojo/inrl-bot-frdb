43f213f02cac2600d8960a362b8f63645261e0b1
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-18 08:22:28 +0530

    fi_x

--- plugins/github.js
+++ plugins/github.js
@@ -1,5 +1,6 @@
-const { inrl } = require("../lib/perfix");
+const { inrl } = require("../lib");
 const inRl = require("../config");
+const { stickersearch } = require("../lib/set_mension");
 inrl(
   {
     pattern: ["git"],
@@ -9,4 +10,17 @@ inrl(
   },
   async (message, client) => {
     await client.sendMessage(message.from,{ text : inRl.GIT }, { quoted: message });
+});
+inrl(
+  {
+    pattern: ["search"],
+    desc: "to check the bot status",
+    sucReact: "🥰",
+    category: ["system", "all"],
+  },
+  async (message, client, match) => {
+if(match == "sticker"){
+let data = await stickersearch(match.split(',')[0])
+    await client.sendMessage(message.from,{ text : data }, { quoted: message });
+    }
 });
