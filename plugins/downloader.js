5e05b712e42d61604688e53b50cd4e47ba570a7d
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-11-23 17:15:27 +0530

    add

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -1,7 +1,6 @@
 //created by @inrl
 //more featurs comming soon
-//fixed, testing
-const { inrl, isUrl, googleIt, wikiMedia, ringTone } = require('../lib');
+const { inrl, isUrl, googleIt, wikiMedia, ringTone, getYtV, getYtA, ytmp4, ytmp3, weather, movie } = require('../lib');
 const Config = require('../config');
 const util = require('util');
 
@@ -57,4 +56,59 @@ inrl(
         return await client.sendMessage( message.from, { audio: { url: result.audio }, fileName: result.title+'.mp3', mimetype: 'audio/mpeg' }, { quoted: message })
           }
      }
+);
+
+inrl(
+	   {
+		pattern: ['ytmp4'],
+		desc: 'To get yt video',
+                sucReact: "💯",
+                category: ["system", "all", "downloade"],
+	   },
+	async (message, client,match) => {
+if(!match.includes('http')){
+await getYtV(message, client)
+}if(match.includes('http')){
+await ytmp4(message, client)
+    }
+  }
+);
+inrl(
+	   {
+		pattern: ['ytmp3'],
+		desc: 'get yt video as mp3 output',
+                sucReact: "🤙",
+                category: ["system", "all", "downloade"],
+	   },
+	async (message, client,match) => {
+if(!match.includes('http')){
+await getYtA(message, client)
+}if(match.includes('http')){
+await ytmp3(message, client)
+    }
+  }
+);
+inrl(
+	   {
+		pattern: ['movie'],
+		desc: 'To get detiles of movie',
+                sucReact: "💥",
+                category: ["system", "all", "downloade"],
+	   },
+	async (message, client,match) => {
+if(!match) return message.send("enter name of movie");
+await movie(message,client);
+  }
+);
+
+inrl(
+	   {
+		pattern: ['weather'],
+		desc: 'To get detiles of you place',
+                sucReact: "🔥",
+                category: ["system", "all"],
+	   },
+	async (message, client,match) => {
+await wather(message,client);
+  }
 );
