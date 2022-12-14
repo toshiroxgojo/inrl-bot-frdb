bf125d8aba2e1f9f52e2afbc4540017ba28918ba
ɪ ɴ ʀ ʟ <fasweehmon2@gmail.com>
2022-12-14 22:12:06 +0530

    Create warn.js

---
+++ plugins/warn.js
@@ -0,0 +1,48 @@
+//created by @inrl
+
+const Config = require('../config');
+const { inrl } = require('../lib')
+const { setWarn, ResetWarn, ListWarn } = require('../lib/database/warndb')
+inrl({
+	pattern: ['warn'],
+	desc: 'To warn a user in group',
+        sucReact: "😑",
+        category: ["system", "all"],
+	},async (message, client, match) => {
+        if(message.client.isCreator && message.isGroup) {
+    const u = message.quoted.sender;
+    const g = message.from;
+    let t = match || "warning";
+    const d = await setWarn(u, g, t)
+    let count = d.count, COUND = Config.WARNCOUND;
+    let remains = COUND - count;
+    let warnmsg = `❏─────[ ᴡᴀʀɴɪɴɢ ]─────❏
+│ Group:-${d.group}
+│ User :-${d.user}
+❏─────────────────╯
+┏─────── INFO ───────❏
+│ Reason :- ${d.reason}
+│ Count :- ${d.count}
+│ Remaining :- ${remains}
+┗•─────────────────❏`
+  await message.reply(warnmsg)
+  if(remains <= "0"){
+  await client.groupParticipantsUpdate( message.from, [message.quoted.sender], "remove" );
+  return await message.reply("_user removes from the group due to warn limit existence_")
+       };
+   };
+})
+inrl({
+	pattern: ['resetwarn'],
+	desc: 'To remove warn count of a user',
+        sucReact: "💥",
+        category: ["system", "all"],
+	},async (message, client, match) => {
+        if(message.client.isCreator && message.isGroup) {
+    const u = message.quoted.sender;
+    const g = message.from;
+    const t = match || "reset";
+    const d = await ResetWarn(u, g, t)
+    return await message.reply(`_reset ${u} your warn count in ${g}_`);
+       };
+});
