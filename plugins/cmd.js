012d45a1f2342486e4c7dc1e8f3e1a6e14978b69
ɪ ɴ ʀ ʟ <fasweehmon2@gmail.com>
2022-12-15 18:51:34 +0530

    fix ip mismatch & nonresponsing!

---
+++ plugins/cmd.js
@@ -0,0 +1,31 @@
+const { inrl } = require("../lib")
+const { setCmd, DeleteCmd } = require('../lib/database/cmddb');
+
+inrl(
+	   {
+		pattern: ['setcmd'],
+		desc: 'To set media as a cmd',
+                sucReact: "💯",
+                category: ["system", "all"],
+	   },
+	async (message, client, match) => {
+    if(!message.quoted) return message.send("replay to a media")
+    if(!message.quoted.msg.fileSha256) return message.send("you replyed not fora media")
+    if(!match) return await message.send("for which cmd!")
+    await setCmd(message.quoted.msg.fileSha256.join(""), match)
+    return await message.reply("successfull")
+	 }
+);
+inrl(
+	   {
+		pattern: ['dltcmd'],
+		desc: 'To dlt media d as alrdy set',
+                sucReact: "💯",
+                category: ["system", "all"],
+	   },
+    async (message, client, match) => {
+    if(!match) return await message.send("which cmd!")
+    await DeleteCmd(match)
+    return await message.reply("successfull")
+	 }
+);
