dffc37b5a79e473bdc4104c2889693979c59c570
ɪ ɴ ʀ ʟ <fasweehmon2@gmail.com>
2022-12-27 14:53:50 +0530

    fix

--- plugins/cmd.js
+++ plugins/cmd.js
@@ -5,13 +5,14 @@ inrl(
 	   {
 		pattern: ['setcmd'],
 		desc: 'To set media as a cmd',
-                sucReact: "💯",
+                sucReact: "😛",
                 category: ["system", "all"],
+                type : "system"
 	   },
-	async (message, client, match) => {
+	async (message, client, match, cmd) => {
     if(message.client.isCreator){
     if(!message.quoted) return message.send("replay to a media")
-    if(!message.quoted.msg.fileSha256) return message.send("you replyed not fora media")
+    if(!message.quoted.msg.fileSha256) return message.send("you replayed not for a media")
     if(!match) return await message.send("for which cmd!")
     await setCmd(message.quoted.msg.fileSha256.join(""), match)
     return await message.reply("successfull")
@@ -22,10 +23,11 @@ inrl(
 	   {
 		pattern: ['dltcmd'],
 		desc: 'To dlt media d as alrdy set',
-                sucReact: "💯",
+                sucReact: "💥",
                 category: ["system", "all"],
+                type : "system"
 	   },
-    async (message, client, match) => {
+    async (message, client, match, cmd) => {
     if(message.client.isCreator){
     if(!match) return await message.send("which cmd!")
     await DeleteCmd(match)
