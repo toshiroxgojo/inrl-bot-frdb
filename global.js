4938cfd673a6da60e3a339dbd042c9515af256e5
ɪ ɴ ʀ ʟ <fasweehmon2@gmail.com>
2023-01-01 16:55:30 +0530

    Delete global.js

--- global.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs")
-global.owner = ["917593919575"];
-global.mess = (type, m) => {
-    let msg = { wait: 'Wait, in progress', media: 'Reply media', error: "No Results Found" }[type]
-    if (msg) return m.reply(msg, m.from, { quoted: m })
-}
