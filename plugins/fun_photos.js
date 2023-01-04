64ef09fccf5ab7585dbd5f9910e63491c98d15ce
ɪ ɴ ʀ ʟ <fasweehmon2@gmail.com>
2023-01-04 07:06:09 +0530

    try new!

--- plugins/fun_photos.js
+++ plugins/fun_photos.js
@@ -3,45 +3,15 @@ const { inrl, dogphoto, lovephoto, cartoonphoto } = require('../lib');
 const fs = require('fs');
 const Config = require('../config');
 
-inrl({ pattern: ['dogphoto'], desc: "thus send random fun dog imgs, maybe bad",sucReact: "💗",  category: ["anime"],}, async (message, client) => {
+inrl({ pattern: ['sdog'], desc: "thus send random fun dog imgs, maybe bad",sucReact: "💗",  category: ["anime"],type: 'sticker'}, async (message, client) => {
 let ttimg = await dogphoto();
-let buttons = [
-        {buttonId:'.dogphoto', buttonText: {displayText: `ɴᴇxᴛ ➪`}, type: 1},
-      ]
-      let buttonMsg = {
-      image: {url:ttimg},
-      caption:  `HeHe!`,
-      footer: Config.FOOTER,
-      buttons: buttons,
-      headerType: 4
-      }
-await client.sendMessage(message.from, buttonMsg, {quoted: message})
+return await client.sendImageAsSticker(message.from, ttimg, message, { packname: Config.PACKNAME, author: Config.FOOTER, categories: '🥵' })
 })
-inrl({ pattern: ['lovephoto'], desc: "thus send random love photos,imgs, maybe bad",sucReact: "💗",  category: ["anime"],}, async (message, client) => {
+inrl({ pattern: ['slove'], desc: "thus send random love photos,imgs, maybe bad",sucReact: "💗",  category: ["anime"],type: 'sticker'}, async (message, client) => {
 let ttimg = await lovephoto();
-let buttons = [
-        {buttonId:'.lovephoto', buttonText: {displayText: `ɴᴇxᴛ ➪`}, type: 1},
-      ]
-      let buttonMsg = {
-      image: {url:ttimg},
-      caption:  `HeHe!!`,
-      footer: Config.FOOTER,
-      buttons: buttons,
-      headerType: 4
-      }
-await client.sendMessage(message.from, buttonMsg, {quoted: message})
+return await client.sendImageAsSticker(message.from, ttimg, message, { packname: Config.PACKNAME, author: Config.FOOTER, categories: '🥵' })
 })
-inrl({ pattern: ['cartoonphoto'], desc: "thus send random cartoon imgs, maybe bad",sucReact: "💗",  category: ["anime"],}, async (message, client) => {
+inrl({ pattern: ['scartoon'], desc: "thus send random cartoon imgs, maybe bad",sucReact: "💗",  category: ["anime"],type: 'sticker'}, async (message, client) => {
 let ttimg = await cartoonphoto();
-let buttons = [
-        {buttonId:'.cartoonphoto', buttonText: {displayText: `ɴᴇxᴛ ➪`}, type: 1},
-      ]
-      let buttonMsg = {
-      image: {url:ttimg},
-      caption:  `HeHe!!!`,
-      footer: Config.FOOTER,
-      buttons: buttons,
-      headerType: 4
-      }
-await client.sendMessage(message.from, buttonMsg, {quoted: message})
+return await client.sendImageAsSticker(message.from, ttimg, message, { packname: Config.PACKNAME, author: Config.FOOTER, categories: '🥵' })
 })
