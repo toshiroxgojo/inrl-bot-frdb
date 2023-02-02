ac6f9a54f74abc1adedc2d530615944be447979b
ɪ ɴ ʀ ʟ <fasweehmon2@gmail.com>
2023-02-02 17:37:32 +0530

    fix

--- plugins/bgmbot.js
+++ plugins/bgmbot.js
@@ -1,21 +1,31 @@
-const {BGMBOT} = require('../config');
+const {BGMBOT,PERFIX} = require('../config');
 const bgm = require('../media/bgm.json');
-
-
+let perfix = PERFIX =='false' ? '': PERFIX;
 module.exports = async(msg, conn, m, store) => {
- let audios=[], mp3;
+if(m.client.body.startsWith(perfix)) return;
+ let audios=[], mp3, chk=[];
+ let add = m.client.body.toLowerCase().trim();
+ if(add.includes(' ')){
+ add = add.split(' ');
+    add.map((p)=>{
+    chk.push(p)
+    })
+    } else {
+    chk.push(add)
+  }
  if(BGMBOT !="true") return;
  for (let key in bgm) {
  let a = [];a.push(key)
- a.map((a)=>{
-  if(m.client.body.toLowerCase().includes(a.toLowerCase())){
+  a.map((a)=>{
+  chk.forEach((c)=>{
+  if(c==a){
   mp3 = bgm[key];
   audios.push(mp3)
   mp3 = audios[Math.floor(Math.random() * audios.length)];
-       }
+           }
+       })
     })
   }
- //return m.reply(audio)
-if(mp3 === undefined) return;
+ if(mp3 === undefined) return;
  return conn.sendMessage(m.from,{ audio: { url: mp3.trim() }, mimetype: "audio/mp4",ptt: true}, { quoted: m } );
 }
