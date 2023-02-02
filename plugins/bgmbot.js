29eb55d3a7a0f94a90ccf6229c9d1259a0412cf5
ɪ ɴ ʀ ʟ <fasweehmon2@gmail.com>
2023-02-02 13:25:21 +0530

    Update bgmbot.js
    Fisk

--- plugins/bgmbot.js
+++ plugins/bgmbot.js
@@ -3,17 +3,18 @@ const bgm = require('../media/bgm.json');
 
 
 module.exports = async(msg, conn, m, store) => {
-      let audios=[], audio;
-if(BGMBOT !="true") return;
-for (let key in bgm) {
-      let a = [];a.push(key)
-      a.map((a)=>{
-if(m.client.body.toLowerCase().includes(a.toLowerCase())){
-      audio = bgm[key];
-      audios.push(audio);
-      audio = audios[Math.floor(Math.random() * audios.length)];
+ let audios=[], mp3;
+ if(BGMBOT !="true") return;
+ for (let key in bgm) {
+ let a = [];a.push(key)
+ a.map((a)=>{
+  if(m.client.body.toLowerCase().includes(a.toLowerCase())){
+  mp3 = bgm[key];
+  audios.push(mp3)
+  mp3 = audios[Math.floor(Math.random() * audios.length)];
        }
     })
   }
- return conn.sendMessage(m.from,{ audio: { url: audio }, mimetype: "audio/mp4",ptt: true}, { quoted: m });
+ //return m.reply(audio)
+ return conn.sendMessage(m.from,{ audio: { url: mp3.trim() }, mimetype: "audio/mp4",ptt: true}, { quoted: m } );
 }
