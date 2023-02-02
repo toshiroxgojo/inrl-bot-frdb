124710cac5b8df0f440445882269f1306d586f48
ɪ ɴ ʀ ʟ <fasweehmon2@gmail.com>
2023-02-02 12:56:27 +0530

    getOne

--- plugins/bgmbot.js
+++ plugins/bgmbot.js
@@ -1,15 +1,19 @@
 const {BGMBOT} = require('../config');
 const bgm = require('../media/bgm.json');
 
+
 module.exports = async(msg, conn, m, store) => {
+      let audios=[], audio;
 if(BGMBOT !="true") return;
- for (let key in bgm) {
-      let a = []; a.push(key)
+for (let key in bgm) {
+      let a = [];a.push(key)
       a.map((a)=>{
-      if(m.client.body.toLowerCase().includes(a.toLowerCase())){
-  let audio = bgm[key];
-  return conn.sendMessage(m.from,{ audio: { url: audio }, mimetype: "audio/mp4",ptt: true}, { quoted: m } );
+if(m.client.body.toLowerCase().includes(a.toLowerCase())){
+      audio = bgm[key];
+      audios.push(audio);
+      audio = audios[Math.floor(Math.random() * audios.length)];
        }
     })
   }
+ return conn.sendMessage(m.from,{ audio: { url: audio }, mimetype: "audio/mp4",ptt: true}, { quoted: m });
 }
